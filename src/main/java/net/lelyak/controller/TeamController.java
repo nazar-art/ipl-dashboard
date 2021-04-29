d403ab2af3232ae015fde2d5b5d45af26876bb10
Nazar lelyak <nazar.lelyak@globallogic.com>
2021-04-29 18:10:58 +0300

    Team page with rest call to backend

--- src/main/java/net/lelyak/controller/TeamController.java
+++ src/main/java/net/lelyak/controller/TeamController.java
@@ -8,6 +8,7 @@ import net.lelyak.repository.TeamRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Nazar Lelyak.
  */
 @Slf4j
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class TeamController {
